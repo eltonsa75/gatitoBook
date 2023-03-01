f4a1bf769324c0ea6d81f3c4bd4fe864449d7c36
Elton Andrade <eltonsa75>
2023-03-01 19:58:52 -0300

    Consumindo o serviço de autenticação

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,27 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  usuario='';
+  senha='';
+
+  constructor(private authService: AutenticacaoService) {}
+
+  login() {
+   this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+    console.log('Atenticado com sucesso');
+   },
+   (error) => {
+    alert('Usuário ou senha inválido');
+    console.log(error);
+   }
+   );
+  }
+
+}
