68f1fcdd7a362ffedb1af60c5379eb5fff14201c
Elton Andrade <eltonsa75>
2023-03-04 02:19:31 -0300

    Criando token

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,9 +1,10 @@
-import {UsuarioExisteService } from './../usuario-existe.service';
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,17 +18,24 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private novoUsuarioService:NovoUsuarioService,
-    private usuarioExistenteService: UsuarioExisteService)
+    private usuarioExisteService: UsuarioExisteService)
     {}
 
     ngOnInit(): void {
-      this.novoUsuarioForm = this.formBuilder.group({
-        email:['', [Validators.required, Validators.email]],
-        fullName:['', [Validators.required, Validators.minLength(4)]],
-        userName:['',[minusculoValidator],[this.usuarioExistenteService.usuarioJaExiste()]],
-        password:[''],
-      },
-
+      this.novoUsuarioForm = this.formBuilder.group(
+        {
+          email: ['', [Validators.required, Validators.email]],
+          fullName: ['', [Validators.required, Validators.minLength(4)]],
+          userName: [
+            '',
+            [Validators.required, minusculoValidator],
+            [this.usuarioExisteService.usuarioJaExiste()],
+          ],
+          password: ['', [Validators.required]],
+        },
+        {
+          validators: [usuarioSenhaIguaisValidator],
+        }
       );
     }
 
