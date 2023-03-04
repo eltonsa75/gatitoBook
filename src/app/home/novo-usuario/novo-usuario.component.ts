b53d83077ec4af2bdee2bb75fa796556075f5320
Elton Andrade <eltonsa75>
2023-03-04 00:32:09 -0300

    Validando letras minusculas, validação assicrona

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,9 @@
+import {UsuarioExisteService } from './../usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -14,15 +16,19 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService:NovoUsuarioService){}
+    private novoUsuarioService:NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService)
+    {}
 
     ngOnInit(): void {
       this.novoUsuarioForm = this.formBuilder.group({
         email:['', [Validators.required, Validators.email]],
         fullName:['', [Validators.required, Validators.minLength(4)]],
-        userName:[''],
+        userName:['',[minusculoValidator],[this.usuarioExistenteService.usuarioJaExiste()]],
         password:[''],
-      });
+      },
+
+      );
     }
 
     cadastrar(){
